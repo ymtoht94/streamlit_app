0d41e7e707e3098b9a074c60fa47c9019a5ec838
ymtoht94 <t125023@ed.sus.ac.jp>
2026-01-26 19:44:58 +0900

    app.py

--- app.py
+++ app.py
@@ -9,18 +9,3 @@ df = pd.read_csv('')
 
 with st.sidebar:
     st.subheader('')
-    #hello
-=======
-import streamlit as st
-import pandas as pd
-import plotly.express as px
-
-st.title('子どもの学校別学習費調査')
-
-df = pd.read_csv('')
-
-with st.sidebar:
-    st.subheader('')
-    
->>>>>>> ab5372693e8ccfaea5db7d3dfbea55dc22b5901f
-st.write('Hello')
